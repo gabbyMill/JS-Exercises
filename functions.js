641a833324e67ac5a0eddd87d67674fb4fdc4bc0
gabbyMill <gabbymillatiner@gmail.com>
2021-08-31 16:57:00 +0300

    halfRange function
    same as range function but sliced first half away.
    Problem didn't specify what to do about odd numbered arrays

--- functions.js
+++ functions.js
@@ -4,3 +4,13 @@ const grades2 = [1,2,3,4,5,6,7,8,9];
 function range (array) {
     return Math.max(...array) - Math.min(...array) // saw this possible with .apply as well but spread looks better   
 }
+
+function halfRange (array) {
+    // Let's first split the grades into first and second half:
+    // I've decided that if an array consists an odd number of indexes ill include the middle as the second half using Math.floor
+    let secondHalf = array.slice(Math.floor(array.length/2))
+    // this will store a copy of the second half of the array
+    let answer = Math.max(...secondHalf) - Math.min(...secondHalf)
+    return answer
+}
+
