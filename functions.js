bb3ec19e0816f2a2b32ed64a4ecdcabe820f2b3c
gabbyMill <gabbymillatiner@gmail.com>
2021-08-31 17:18:28 +0300

    median function
    used sort algorithm found on MDN and
    template literals + string interpolation.
    Very convenient.

--- functions.js
+++ functions.js
@@ -1,6 +1,4 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
-const grades2 = [1,2,3,4,5,6,7,8,9];
-
 function range (array) {
     return Math.max(...array) - Math.min(...array) // saw this possible with .apply as well but spread looks better   
 }
@@ -13,4 +11,26 @@ function halfRange (array) {
     let answer = Math.max(...secondHalf) - Math.min(...secondHalf)
     return answer
 }
+
+function median (array) {
+// MDN: callback function for sort method:
+let sorted = array.sort(function(a, b) {
+    return a - b;
+});
+    // if even numbered then median is average of two medians:
+    if (array.length % 2 === 0) {
+        return (sorted[sorted.length/2] + sorted[sorted.length/2 - 1])/2
+    }
+    else {
+        return sorted[Math.floor(sorted.length/2)]
+    }
+}
+
+console.log(`Stats:
+    Range: ${range(grades)}
+    Half Range: ${halfRange(grades)}
+    Median: ${median(grades)}`);
+
+    // I put Half Range before median because slice alters the original array;
+    // If I wanted the orignal to stay the same I'd have done something like let newArr = array; and worked with newArr
 
