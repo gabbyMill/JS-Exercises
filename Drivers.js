6a88991ce435a96fb556622dd2581c29a0ad3aeb
gabbyMill <gabbymillatiner@gmail.com>
2021-09-01 10:00:54 +0300

    Fix
    Fix to more concise and simpler code using bracket notation

--- Drivers.js
+++ Drivers.js
@@ -1,4 +1,3 @@
-// list of Formula 1 champions since the year 2000
 const formula1Champions = [
     "Schumacher",
     "Schumacher",
@@ -22,42 +21,15 @@ const formula1Champions = [
     "Hamilton",
     "Hamilton"
 ]
-let winners = {}
+let winners = {};
 
 function countWin(driver) {
     // your code goes here
-    if (winners.Hamilton === undefined) {
-        winners.Hamilton = 0
+    let driverName = driver;
+    if (!winners.hasOwnProperty(driver)) {
+        winners[driverName] = 0;
     }
-    if (winners.Vettel === undefined) {
-        winners.Vettel = 0
-    }
-    if (winners.Räikkönen === undefined) {
-        winners.Räikkönen = 0
-    }
-    if (winners.Alonso === undefined) {
-        winners.Alonso = 0
-    }
-    if (winners.Rosberg === undefined) {
-        winners.Rosberg = 0
-    }
-    if (winners.Schumacher === undefined) {
-        winners.Schumacher = 0
-    }
-    if (winners.Button === undefined) {
-        winners.Button = 0
-    }
-    if (driver === "Vettel") {winners.Vettel += 1}
-    if (driver === "Schumacher") {winners.Schumacher += 1}
-    if (driver === "Button") {winners.Button += 1}
-    if (driver === "Rosberg") {winners.Rosberg += 1}
-    if (driver === "Alonso") {winners.Alonso += 1}
-    if (driver === "Räikkönen") {winners.Räikkönen += 1}
-    if (driver === "Hamilton") {winners.Hamilton += 1}
+    winners[driverName]++;
 }
-
 formula1Champions.forEach(countWin)
-console.log(winners);
-
-// The above code calls the countWin function for each of the drivers in the list of champions.
-
+console.log(winners)
